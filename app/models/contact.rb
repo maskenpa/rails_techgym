3a4c89f25d1e8b5e0a64e84405d80de494272ca4
maskenpa <masukawa_1001@yahoo.co.jp>
2020-09-16 23:33:03 +0900

    add comment in contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,8 +2,11 @@ class Contact < ApplicationRecord
   enum title: %i(  service relationship job other )
   enum status: %i( yet done )
   
+  #new_record?はDBにレコードが登録されていなければtrue,登録されていればfalseとなる
+  #active recordではモデルが特定の動作をする際にコールバックを設定することができる
   after_initialize :set_default, if: :new_record?
-
+  
+  #||=は自己代入演算子、何も入っていなければ代入する
   def set_default
     self.status ||= :yet
   end
