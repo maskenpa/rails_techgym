abe59cdf09e538834e96d1fe9d574e54fcf95556
maskenpa <masukawa_1001@yahoo.co.jp>
2020-09-19 23:30:14 +0900

    fix form_with bug

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,16 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new()
   end
 
   def create
+    puts @contacts
   end
 
   def thanks 
   end
-
+private
+  def contact_params
+    params.require(:contact).permit(:title, :name, :email, :content, :status)
+  end
 end
