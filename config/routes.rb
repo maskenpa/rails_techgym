d4a685f2abaa8ed51f41e808a87071036a6c721d
maskenpa <masukawa_1001@yahoo.co.jp>
2020-12-19 14:18:33 +0900

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => 'home#top'
   get '/contacts/new' => 'contacts#new'
   get '/contacts/create' => 'contacts#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
